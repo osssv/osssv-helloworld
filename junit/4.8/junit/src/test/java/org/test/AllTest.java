3f1556c1eb83668ecfa1c5a318f177607e989e93
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2014-11-22 12:25:39 -0800

    Add test suite code

---
+++ junit/4.8/junit/src/test/java/org/test/AllTest.java
@@ -0,0 +1,10 @@
+package org.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AppTest.class, CalcTest.class })
+public class AllTest {
+}
