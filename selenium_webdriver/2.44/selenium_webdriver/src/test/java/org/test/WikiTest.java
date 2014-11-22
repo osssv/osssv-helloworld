68c6c66b45375dd0225f637d85b6f684be0c3f89
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2014-11-21 20:38:14 -0800

    Create Selenium directory

---
+++ selenium_webdriver/2.44/selenium_webdriver/src/test/java/org/test/WikiTest.java
@@ -0,0 +1,76 @@
+package org.test;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class WikiTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.virago.sakura.ne.jp/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testSample() throws Exception {
+    driver.get(baseUrl + "/osssv/public/index.php?HowToUse%2FSelenium%20IDE%2F2.8");
+    driver.findElement(By.linkText("Search")).click();
+    driver.findElement(By.name("word")).clear();
+    driver.findElement(By.name("word")).sendKeys("StudySession");
+    driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+    assertEquals("MenuBar", driver.findElement(By.linkText("MenuBar")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
